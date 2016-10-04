4e6262889785a82d83e6b7fc61bf0d64ac845269
RyuPechi <programeraiza@hotmail.co.jp>
2016-10-04 14:44:40 +0900

    add 4-*

---
+++ donyu2/review4-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+int main(void) {
+	int i;
+	double x;
+	srand((unsigned)time(NULL));
+	for (i = 0; i<10; i++) {
+		x = (double)rand()/RAND_MAX;
+		printf("%f\n", x);
+	}
+	return(0);
+}
+
