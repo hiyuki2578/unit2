0527743f89e25804d0abed507647e68d0ca69c06
RyuPechi <programeraiza@hotmail.co.jp>
2016-10-06 11:49:06 +0900

    add review5-*

---
+++ donyu2/review5-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(void) {
+	int no;
+	printf("®”’l‚ð“ü—Í = ");
+	scanf("%d", &no);
+	for (no;no>=1;no--){
+		printf("%d ", no);
+	}
+	return 0;
+}
---
+++ donyu2/review5-2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	int i,no[5] = {10,3,5,6,8};
+	double sum = 0;
+		for (i = 0; i<5; i++){
+			sum = sum + no[i];
+		}
+	sum = sum / i;
+	printf("•½‹Ï’l%f",sum);
+	return 0;
+}
