6b192e4b624f98456992f6c43420f697e60544b2
RyuPechi <programeraiza@hotmail.co.jp>
2016-10-11 04:47:36 +0000

    add review6-*

---
+++ donyu2/review6-1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+    
+    char ch='y';
+    printf("%c",ch);
+	return 0;
+}
---
+++ donyu2/review6-2.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main(void){
+ char ch1;
+char ch2=0x4E;
+ //文字を 16 進数アスキーコードで入力している
+ printf("１文字入力してください。¥n");
+ scanf("%c", &ch1); getchar();
+ if( ch1 == ch2 ) { printf("正解！¥n"); } // =は 2 つです
+ else { printf("残念・・・¥n"); }
+ return 0;
+}
