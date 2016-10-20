be3931f3b7fb3470aa9f25738042e9504e7b8601
RyuPechi <programeraiza@hotmail.co.jp>
2016-10-20 03:04:58 +0000

    add review1-9-*

---
+++ donyu2/review1-9-3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <ctype.h>
+int main(void) {
+    char str[12];
+    int i;
+    printf("文字列を入力してください\n");
+    gets(str);
+    printf("大文字に変換します\n");
+    for(i=0;i<=12;i++){
+        str[i]=toupper(str[i]);
+    }
+    printf("%s\n",str);
+    return 0;
+}
