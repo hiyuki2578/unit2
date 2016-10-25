37b629a85c4dbd59bdfcfc05d3a1ddf1583bd197
RyuPechi <programeraiza@hotmail.co.jp>
2016-10-25 18:52:07 +0900

    add test

---
+++ donyu2/test1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <time.h>
+int main(void) {
+	int i, count = 0;
+	double avg, x;
+	______
+	printf("�ŗ�\n");	scanf("%lf",&avg);
+	for (i = 0;i < 9;i++) {
+		x=
+			if (x < avg) {
+
+				count++;
+			}
+			else {
+
+			}
+			printf("HIT��%d�{", count);
+	}
+return 0;
+}
