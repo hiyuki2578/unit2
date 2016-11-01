a6121498c9236287f87b65973025d25fc5058784
RyuPechi <programeraiza@hotmail.co.jp>
2016-11-01 05:02:25 +0000

    add file

---
+++ donyu2/review1-10-1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void) {
+    int a;
+    FILE *fp;
+    if((fp = fopen("text411.txt","w")) == NULL){
+        printf("ファイルを開けませんでした。\n");
+        return 1;
+    }
+        fprintf(fp, "%d",a);
+        fclose(fp);
+        return (0);
+}
---
+++ donyu2/review1-10-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    int a;
+    FILE *fp;
+    if((fp = fopen("text411.txt","r")) == NULL){
+        printf("ファイルを開けませんでした。\n");
+        return 1;
+    }
+        fscanf(fp,"%d",&a);
+        fclose(fp);
+        printf("%d",a);
+        return (0);
+}
