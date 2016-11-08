bb8501b5f400a069d0d6fd54c5cf2e63df323272
RyuPechi <programeraiza@hotmail.co.jp>
2016-11-08 05:06:36 +0000

    add files

---
+++ donyu2/review1-11-2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void) {
+    double a;
+    FILE *fp;
+    if((fp = fopen("text412.txt","r")) == NULL){
+        printf("値を読み込めませんでした。\n");
+        return 1;
+    }
+        fscanf(fp,"%lf",&a);
+        fclose(fp);
+        printf("%f",a);
+        return (0);
+}
