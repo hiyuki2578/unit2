000088bf3375c3ade917dc8bedf7819fc06d9730
RyuPechi <programeraiza@hotmail.co.jp>
2016-11-10 02:41:06 +0000

    add files

---
+++ donyu2/review1-12-1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(void) {
+    FILE *fp;
+    char str[]={"Hello!\n"};
+    if((fp = fopen("text431.txt","w")) == NULL){
+        printf("ファイルを開けませんでした。\n");
+        return 1;
+    }
+    
+        fprintf(fp,"%s\n",str);
+        fclose(fp);
+        return (0);
+}
+
