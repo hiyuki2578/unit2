634f1d4eebdc78bceea0434c0101b2159be12c06
RyuPechi <programeraiza@hotmail.co.jp>
2016-12-01 02:53:31 +0000

    add files

--- donyu2/review1-17-3.c
+++ donyu2/review1-17-3.c
@@ -5,25 +5,18 @@
    double weight;
    }data;
    int main(void){
-    struct data student[100] = {
-    printf("名前\n");scanf("%s,&student[1].name");
-    printf("身長\n");scanf("%d,&student[1].height");
-    printf("体重\n");scanf("%s,&student[1].weight");
-    
-    printf("名前\n");scanf("%s,&student[2].name");
-    printf("身長\n");scanf("%d,&student[2].height");
-    printf("体重\n");scanf("%s,&student[2].weight");
-    
-    
-    {"Jun" , 170, 65.0 },
-    {"Nero", 160, 51.2 }
-    };
-    printf("名前=%s\n",student[1].name);
-    printf("身長=%d[cm]\n",student[1].height);
-    printf("体重=%f[kg]\n",student[1].weight);
-    
-    printf("名前=%s\n",student[1].name);
-    printf("身長=%d[cm]\n",student[1].height);
-    printf("体重=%f[kg]\n",student[1].weight);
-   return 0;
+    data student[100];
+    int i;
+    for(i=0;i<2;i++){
+     printf("名前\n");scanf("%s",student[i].name);
+     printf("身長\n");scanf("%d",&student[i].height);
+     printf("体重\n");scanf("%lf",&student[i].weight);
+     printf("\n");
+     }
+    for(i=0;i<2;i++){
+    printf("名前=%s\n",student[i].name);
+    printf("身長=%d[cm]\n",student[i].height);
+    printf("体重=%f[kg]\n",student[i].weight);
+    }
+    return (0);
   }
