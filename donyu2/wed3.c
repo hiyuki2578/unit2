882fdd3e368406836abf64c9de573b3f74de2de1
hiyuki2578 <tomoka319@gmail.com>
2016-12-07 13:17:22 +0900

    Null resolve #8

--- donyu2/wed3.c
+++ donyu2/wed3.c
@@ -1,20 +1,28 @@
 #include<stdio.h>
-typedef struct {
-	char name[20];
-	int point;
+typedef struct{
+	char	name[20];
+	int	number;
+	double	height;
+	double	weight;
 }data;
-void print_point (data student) {
-	printf("%sの点数は%d点です\n",student.name,student.point);
-}
-int main(void){
+int main(){
+	data person[5];
+	FILE *fp;
 	int i;
-	data person[3]={
-		{"yamada",92},
-		{"sato",85},
-		{"takahashi",66},
-	};
-	for(i=0;i<3;i++){
-		print_point(person[i]);
+	if((fp=fopen("wed3.txt","r"))==NULL){
+		printf("ファイルを開けませんでした。\n");
+		return 1;
+	}
+	for(i=0;i<5;i++){
+		fscanf(fp,"%[^,],%d,%lf,%lf",person[i].name,&person[i].number,&person[i].height,&person[i].weight);
+	}
+	fclose(fp);
+	for(i=0;i<5;i++){
+		printf("名前:%s\n",person[i].name);
+		printf("学籍番号:%d\n",person[i].number);
+		printf("身長:%f\n",person[i].height);
+		printf("体重:%f\n",person[i].weight);
 	}
 	return 0;
 }
+
